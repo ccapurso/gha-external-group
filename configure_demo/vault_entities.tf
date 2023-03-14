bbb2b7566035febed437165c0cf30f60e3565ed9
brightblueray <rryjewski@hashicorp.com>
2023-03-14 16:32:45 -0400

    woot

--- configure_demo/vault_entities.tf
+++ configure_demo/vault_entities.tf
@@ -9,14 +9,30 @@ resource "vault_identity_group" "group" {
   policies = ["ci-secret-reader"]
 }
 
-# # Create Vault Auth Backend
-# resource "vault_auth_backend" "github" {
-#   type = "github"
-# }
-
 # Associate External Group to GitHub Team
 resource "vault_identity_group_alias" "group-alias" {
   name           = "team-se"
   mount_accessor = vault_github_auth_backend.github_auth.accessor
   canonical_id   = vault_identity_group.group.id
+}
+
+# Configure Entities
+resource "vault_identity_entity" "rryjewski" {
+  name = "rryjewski"
+}
+
+resource "vault_identity_entity_alias" "rryjewski_gh" {
+  name = "brightblueray"
+  canonical_id = vault_identity_entity.rryjewski.id
+  mount_accessor = vault_github_auth_backend.github_auth.accessor
+}
+
+resource "vault_identity_entity_alias" "rryjewski_jwt" {
+  name = "brightblueray"
+  canonical_id = vault_identity_entity.rryjewski.id
+  mount_accessor = vault_jwt_auth_backend.jwt.accessor
+}
+
+resource "vault_identity_entity" "bmiller" {
+  name = "brandon.miller"
 }
